20513019b3139b1843252b9c032e6af256ddf947
Douglas Stockwell <doug@11011.net>
2013-10-25 22:17:03 +1100

    Wire editor to buffer
    * use requestAnimationFrame to coalesce updates
    * place and absolutely position a span at the insertion point to represent
      the cursor

--- app/main.js
+++ app/main.js
@@ -1,26 +1,70 @@
-define(['lib/input'], function(input) {
+define(['lib/input', 'lib/buffer'], function(input, Buffer) {
   var host = 'http://localhost:61203/';
   var file = 'app/main.js';
+  var buffer = new Buffer();
+  var cursor = document.querySelector('#cursor');
+  cursor.mark = buffer.mark({row: 0, col: 0});
   var display = document.querySelector('#display');
-  var point = document.querySelector('#point');
   function loadFile() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', host + file);
     xhr.send();
     xhr.onload = function() {
-      display.innerText = xhr.responseText;
-      ensurePoint();
+      var lastRow = buffer.lines.length -1;
+      var lastCol = buffer.lines[lastRow].length;
+      buffer.delete({row: 0, col: 0}, {row: lastRow, col: lastCol});
+      buffer.insert({row: 0, col: 0}, 'after', xhr.responseText);
+      updateDisplay();
     };
+  }
+  var updateScheduled = false;
+  function updateDisplay() {
+    if (updateScheduled) {
+      return;
+    }
+    updateScheduled = true;
+    requestAnimationFrame(function() {
+      updateScheduled = false;
+      var elements = display.children;
+      var lines = buffer.lines;
+      var changes = buffer.changes;
+      for (var i = 0; i < changes.length; i++) {
+        var line = lines[i];
+        if (line == '') {
+          line = ' ';
+        }
+        var change = changes[i];
+        for (var j = 0; j < change.deleted; j++) {
+          elements[i].remove();
+        }
+        if (change.kind == 'inserted') {
+          display.insertBefore(document.createElement('div'), elements[i]);
+        }
+        if (i == cursor.mark.row) {
+          var element = elements[i];
+          var pre = line.substring(0, cursor.mark.col);
+          var post = line.substring(cursor.mark.col);
+          element.textContent = pre;
+          element.appendChild(cursor);
+          element.appendChild(document.createTextNode(post));
+          element.classList.add('cursor-line');
+        } else if (change.kind == 'modified' || change.kind == 'inserted') {
+          elements[i].textContent = line;
+          elements[i].classList.remove('cursor-line');
+        } else {
+          elements[i].classList.remove('cursor-line');
+        }
+      }
+      while (elements.length > lines.length) {
+        elements[elements.length - 1].remove();
+      }
+      buffer.clearChanges();
+    });
   }
   function saveFile() {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', host + file);
     xhr.send(display.innerText);
-  }
-  function ensurePoint() {
-    if (!point.parentNode) {
-      display.insertBefore(point, display.firstChild);
-    }
   }
   function onkey(key) {
     var text = '';
@@ -32,41 +76,43 @@ define(['lib/input'], function(input) {
         text = '\n'
       } else if (keyString == 'Ctrl-R') {
         loadFile();
-        return;
       } else if (keyString == 'Ctrl-C') {
-        display.innerText = '';
-        return;
+        var lastRow = buffer.lines.length -1;
+        buffer.delete(
+            {row: 0, col: 0},
+            {row: lastRow, col: buffer.lines[lastRow].length});
+      } else if (keyString == 'Ctrl-J') {
+        if (cursor.mark.row < buffer.lines.length) {
+          cursor.mark.row++;
+        }
+      } else if (keyString == 'Ctrl-K') {
+        if (cursor.row > 0) {
+          cursor.mark.row--;
+        }
       } else if (keyString == 'Ctrl-H') {
-        if (!point.previousSibling) {
-          return;
+        if (cursor.col > 0) {
+          cursor.mark.col--;
         }
-        if (point.previousSibling.length > 1) {
-          point.previousSibling.splitText(point.previousSibling.length - 1);
-        }
-        point.parentNode.insertBefore(point, point.previousSibling);
-        return;
       } else if (keyString == 'Ctrl-L') {
-        if (!point.nextSibling) {
-          return;
+        if (cursor.mark.col < buffer.lines[cursor.mark.row].length) {
+          cursor.mark.col++;
         }
-        if (point.nextSibling.length > 1) {
-          point.nextSibling.splitText(1);
-        }
-        point.parentNode.insertBefore(point, point.nextSibling.nextSibling);
-        return;
       } else if (keyString == 'Backspace') {
-        if (!point.previousSibling) {
-          return;
+        var prev = {row: cursor.mark.row, col: cursor.mark.col - 1};
+        if (prev.col < 0) {
+          if (prev.row == 0) {
+            return;
+          }
+          prev.row--;
+          prev.col = buffer.lines[prev.row].length;
         }
-        if (point.previousSibling.length > 1) {
-          point.previousSibling.splitText(point.previousSibling.length - 1);
-        }
-        point.previousSibling.remove();
+        buffer.delete(prev, {row: cursor.mark.row, col: cursor.mark.col});
       }
     }
     if (text !== '') {
-      point.insertAdjacentText('beforeBegin', text);
+      buffer.insert({row: cursor.mark.row, col: cursor.mark.col}, 'before', text);
     }
+    updateDisplay();
   }
   
   input.setHandler(onkey);

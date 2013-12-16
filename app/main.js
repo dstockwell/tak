9d4c4e4991ed72df1148d2d8f88479bd3b650225
Douglas Stockwell <doug@11011.net>
2013-12-16 21:14:17 +1100

    WIP - Introduce modal editing

--- app/main.js
+++ app/main.js
@@ -5,70 +5,7 @@ import {Frame} from '../lib/frame.js';
   var file = 'app/main.js';
   var frame = new Frame();
   document.documentElement.appendChild(frame._element);
-  function loadFile() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', host + file);
-    xhr.send();
-    xhr.onload = function() {
-      var lastRow = frame.buffer.lines.length -1;
-      var lastCol = frame.buffer.lines[lastRow].length;
-      frame.buffer.delete({row: 0, col: 0}, {row: lastRow, col: lastCol});
-      frame.buffer.insert({row: 0, col: 0}, 'after', xhr.responseText);
-      frame.update();
-    };
-  }
-  function saveFile() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', host + file);
-    xhr.send(display.innerText);
-  }
-  function onkey(key) {
-    var text = '';
-    if (key.isText()) {
-      text = key.text;
-    } else {
-      var keyString = key.toString();
-      if (keyString == 'Enter' || keyString == 'RightAlt') {
-        text = '\n'
-      } else if (keyString == 'Ctrl-R') {
-        loadFile();
-      } else if (keyString == 'Ctrl-C') {
-        var lastRow = frame.buffer.lines.length -1;
-        frame.buffer.delete(
-            {row: 0, col: 0},
-            {row: lastRow, col: frame.buffer.lines[lastRow].length});
-      } else if (keyString == 'Ctrl-J') {
-        if (frame.cursor.row < frame.buffer.lines.length) {
-          frame.cursor.row++;
-        }
-      } else if (keyString == 'Ctrl-K') {
-        if (frame.cursor.row > 0) {
-          frame.cursor.row--;
-        }
-      } else if (keyString == 'Ctrl-H') {
-        if (frame.cursor.col > 0) {
-          frame.cursor.col--;
-        }
-      } else if (keyString == 'Ctrl-L') {
-        if (frame.cursor.col < frame.buffer.lines[frame.cursor.row].length) {
-          frame.cursor.col++;
-        }
-      } else if (keyString == 'Backspace' || keyString == 'LeftAlt') {
-        var prev = {row: frame.cursor.row, col: frame.cursor.col - 1};
-        if (prev.col < 0) {
-          if (prev.row == 0) {
-            return;
-          }
-          prev.row--;
-          prev.col = frame.buffer.lines[prev.row].length;
-        }
-        frame.buffer.delete(prev, {row: frame.cursor.row, col: frame.cursor.col});
-      }
-    }
-    if (text !== '') {
-      frame.buffer.insert({row: frame.cursor.row, col: frame.cursor.col}, 'before', text);
-    }
+  input.setHandler(function (key) {
+    frame.mode.onkey(key);
     frame.update();
-  }
-  
-  input.setHandler(onkey);
+  });
